8ba25e64e59158f08d06f29000c058c391fd6dc6
DSR4-Guru <srakesh2435tn@gmail.com>
2026-06-22 18:58:22 +0530

    api/chat

--- frontend/src/api/chatApi.js
+++ frontend/src/api/chatApi.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+export default axios.create({
 
-const client = axios.create({
-  baseURL: API_URL,
-  timeout: 25000,
+baseURL:
+
+import.meta.env
+
+.VITE_API_URL
+
 });
 
 /**
